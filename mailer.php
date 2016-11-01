049a7c69d30c15f892c30e88fb040c1c73818a66
Hassan Corrigan <corriganhassan@gmail.com>
2016-11-01 15:44:41 +0000

    Contact pages added, mailer working and final.

---
+++ mailer.php
@@ -0,0 +1,18 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $to = 'hassan.corrigan2@student.ncirl.ie';
+    $from = 'From: Chat with Alex';
+    $subject = 'Contact Form/Bug Fix';
+
+    $body = "From: $name\n E-Mail Address: $email\n Message:\n $message";
+
+    if (!empty($message)) {
+      if (mail ($to, $subject, $body, $from)) {
+        header("Location: " . "contact-success.html");
+      } else {
+  	    header("Location: " . "contact-fail.html");
+      }
+    }
+?>
